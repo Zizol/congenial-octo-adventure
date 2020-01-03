8d7c95641b9dcbe5d2373aa0f9d2e3d582533b26
Zizol <adrien.videgrain@gmail.com>
2020-01-03 19:00:49 +0100

    Updates

---
+++ precache-manifest.c23bb29f3ec0eb95a258684ae7766aef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226c1103f7e67bf6b025146655228e60",
+    "url": "/congenial-octo-adventure/index.html"
+  },
+  {
+    "revision": "47eebf9960fed80bf5e0",
+    "url": "/congenial-octo-adventure/static/css/main.eb82a97a.chunk.css"
+  },
+  {
+    "revision": "361153c69d9fbc567ba4",
+    "url": "/congenial-octo-adventure/static/js/2.32c47b13.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/congenial-octo-adventure/static/js/2.32c47b13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47eebf9960fed80bf5e0",
+    "url": "/congenial-octo-adventure/static/js/main.993572e3.chunk.js"
+  },
+  {
+    "revision": "16312ca29c54fc73e3f4",
+    "url": "/congenial-octo-adventure/static/js/runtime-main.637fae54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/congenial-octo-adventure/static/media/logo.5d5d9eef.svg"
+  }
+]);
